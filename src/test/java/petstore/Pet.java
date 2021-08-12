942cd3b6535b15cc69f0662ba90ecd21cebd314a
Samuel <samuel.andradetp@live.com>
2021-08-12 20:27:12 -0300

    Incluída assertivas para o body do teste

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 // 3 - Classe
 public class Pet {
@@ -39,6 +40,9 @@ public class Pet {
         .then() // Então
                 .log().all()
                 .statusCode(200)
+                .body("name", is("Elvis"))
+                .body("status", is("Available"))
         ;
     }
 }
+//9223372036854775807
