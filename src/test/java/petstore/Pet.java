af9608e0b61fb98f343ac9a4a8f29de45f1d072e
Samuel <samuel.andradetp@live.com>
2021-08-12 22:05:21 -0300

    Finalizado script para excluirPet (Delete)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -2,6 +2,7 @@
 package petstore;
 
 // 2 - Bibliotecas
+
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -9,7 +10,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
 
 // 3 - Classe
 public class Pet {
@@ -18,13 +20,12 @@ public class Pet {
 
     // 3.2 - Métodos e Funções
     public String lerJson(String caminhoJson) throws IOException {
-
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
     // Incluir - Create - Post
-    @Test // Identifica o método ou função como um teste para o TestNG
-    public void incluirPet() throws IOException{
+    @Test(priority = 1)  // Identifica o método ou função como um teste para o TestNG
+    public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
         // Sintaxe Gherkin
@@ -32,17 +33,80 @@ public class Pet {
         // Given - When - Then
 
         given() // Dado
-                .contentType("application/json") // comum em API REST - antigas eram "text/xml"
+                .contentType("application/json") // comum em API REST - antigas era "text/xml"
                 .log().all()
                 .body(jsonBody)
-        .when() // Quando
+        .when()  // Quando
                 .post(uri)
-        .then() // Então
+        .then()  // Então
                 .log().all()
                 .statusCode(200)
-                .body("name", is("Elvis"))
-                .body("status", is("Available"))
+                .body("name", is("Atena"))
+                .body("status", is("available"))
+                .body("category.name", is("AX2345LORT"))
+                .body("tags.name", contains("data"))
+        ;
+
+
+    }
+
+    @Test(priority=2)
+    public void consultarPet(){
+        String petId = "150364";
+
+        String token =
+                given()
+                        .contentType("application/json")
+                        .log().all()
+                .when()
+                        .get(uri + "/" + petId)
+                .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("name", is("Atena"))
+                        .body("category.name", is("AX2345LORT"))
+                        .body("status",is("available"))
+                        .extract()
+                        .path("category.name")
+                ;
+        System.out.println("O token é " + token);
+
+    }
+
+    @Test(priority=3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pet2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Atena"))
+                .body("status",is("sold"))
+        ;
+    }
+
+    @Test(priority=4)
+    public void excluirPet(){
+
+        String petId = "150364";
+
+        given()
+                 .contentType("application/json")
+                 .log().all()
+        .when()
+                 .delete(uri + "/" + petId)
+        .then()
+                 .log().all()
+                 .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(petId))
         ;
     }
 }
-//9223372036854775807
